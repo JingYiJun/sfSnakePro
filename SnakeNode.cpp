28eb73ebb0032dd99452787b6408592cebc1312b
jhpy1024 <jhpy1024@gmail.com>
2013-11-23 21:18:55 +0000

    Implemented SnakeNode functions

--- SnakeNode.cpp
+++ SnakeNode.cpp
@@ -1,3 +1,44 @@
+#include <SFML/Graphics.hpp>
+
 #include "SnakeNode.h"
 
 using namespace sfSnake;
+
+const float SnakeNode::Width = 10.f;
+const float SnakeNode::Height = 10.f;
+
+SnakeNode::SnakeNode(sf::Vector2f position)
+: position_(position)
+{
+	shape_.setPosition(position_);
+	shape_.setFillColor(sf::Color::Green);
+	shape_.setSize(sf::Vector2f(SnakeNode::Width, SnakeNode::Height));
+}
+
+void SnakeNode::setPosition(sf::Vector2f position)
+{
+	position_ = position;
+}
+
+void SnakeNode::setPosition(float x, float y)
+{
+	position_.x = x;
+	position_.y = y;
+}
+
+void SnakeNode::move(float xOffset, float yOffset)
+{
+	position_.x += xOffset;
+	position_.y += yOffset;
+}
+
+sf::Vector2f SnakeNode::getPosition() const
+{
+	return position_;
+}
+
+void SnakeNode::render(sf::RenderWindow& window)
+{
+	shape_.setPosition(position_);
+	window.draw(shape_);
+}
