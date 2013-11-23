acf2debc0edd545dde7c18f444561b66c852f454
jhpy1024 <jhpy1024@gmail.com>
2013-11-23 22:30:52 +0000

    Added Fruit class

---
+++ Fruit.h
@@ -0,0 +1,22 @@
+#ifndef FRUIT_H
+#define FRUIT_H
+
+#include <SFML/Graphics.hpp>
+
+namespace sfSnake
+{
+class Fruit
+{
+public:
+	Fruit(sf::Vector2f position = sf::Vector2f(0, 0));
+
+	void render(sf::RenderWindow& window);
+
+private:
+	sf::CircleShape shape_;
+
+	static const float Radius;
+};
+}
+
+#endif
